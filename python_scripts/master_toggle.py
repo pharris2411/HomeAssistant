b4cd9323e77285cb83cac4643595ce7914ddd5bd
zim2411 <zim2411@gmail.com>
2018-09-16 00:17:03 -0400

    Levels!

--- python_scripts/master_toggle.py
+++ python_scripts/master_toggle.py
@@ -1,8 +1,22 @@
+
+now = datetime.datetime.now()
+hour = now.hour
+
 state = hass.states.get(data['master']).state
 
 if state == 'on':
     hass.services.call('homeassistant', 'turn_off',
                        {'entity_id': data['slave']})
 else:
-    hass.services.call('homeassistant', 'turn_on', {
-                       'entity_id': data['slave']})
+
+	level = 100
+
+	if hour >= 0 and hour < 8:
+		level = 10
+	elif hour >= 8 and hour <= 20:
+		level = 100
+	else:
+		level = 50
+
+	data = { "entity_id" : data['slave'], "brightness" : level }
+	hass.services.call('light', 'turn_on', data)
